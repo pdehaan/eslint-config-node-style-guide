18ef37bfbcca3e85d4b83642cfdb8e93707b2f30
Peter deHaan <peter@deseloper.com>
2015-10-13 23:50:26 -0700

    Code cleanup with loading external .eslintrc file

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 'use strict';
 
-var readFile = require('fs').readFileSync;
-// var fixRules = require('./lib/utils').fixRules;
+var utils = require('./lib/utils');
+var config = utils.loadConfig();
 
-var eslintRules = {};
-
-try {
-  var contents = readFile('./node_modules/node-style-guide/.eslintrc', 'utf-8');
-  eslintRules = JSON.parse(contents || {});
-  // eslintRules = fixRules(eslintRules);
-} catch (err) {
-  console.error(err);
-  console.error('Unable to load node-style-guide/.eslintrc');
-  process.exit(1);
-}
-
-module.exports = eslintRules;
+module.exports = config;
